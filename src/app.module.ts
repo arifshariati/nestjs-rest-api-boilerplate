93ca11c41e31369c1d30d92f678182c5302685aa
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-05-28 14:12:42 +0200

    added basic config for working with .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env.development' : `.env.${ENV}`,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
