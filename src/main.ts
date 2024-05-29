f104cd84a52285f458b1b390d686c7ffe587165e
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-05-29 12:58:37 +0200

    updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
+
+const PORT = parseInt(process.env.PORT, 10) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 4000;
+
+  // register plugins and extensions
+  app.enableCors({ origin: '*' }); // for production: specific IP
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableVersioning({ type: VersioningType.URI });
+  app.use(helmet());
+  app.use(compression());
+
   await app.listen(PORT, () =>
-    Logger.log(`Server is listening on PORT:${PORT}`),
+    Logger.log(`🚀 Server is listening on PORT:${PORT}`),
   );
 }
 bootstrap();
