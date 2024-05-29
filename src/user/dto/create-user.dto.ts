829c91f19c023a8b1217660bd6e084a11df092a0
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-05-29 17:54:41 +0200

    added create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  age: number;
+
+  @IsEmail()
+  email: string;
+}
