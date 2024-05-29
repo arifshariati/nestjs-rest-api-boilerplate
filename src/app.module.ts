b8c1e77b375fdf7af4cd7869eb1e612f874fa3fa
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-05-29 17:55:08 +0200

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ThrottlerModule } from '@nestjs/throttler';
 
-const ENV = process.env.NODE_ENV;
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: !ENV ? '.env.development.local' : `.env.${ENV}.local`,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     ThrottlerModule.forRoot([{ ttl: 60, limit: 10 }]),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: () => ({
-        uri: 'mongodb://localhost:27017/test',
-      }),
-      inject: [ConfigService],
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      dbName: 'test',
+      autoCreate: true,
     }),
     UserModule,
   ],
