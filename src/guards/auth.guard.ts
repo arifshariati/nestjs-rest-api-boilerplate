1afbac904e275786f9f8d3217fb921b7fc3271ad
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-01 07:33:44 +0200

    added @Public logic inside authGuard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -4,15 +4,29 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { jwtConstants } from 'src/auth/constants';
+import { IS_PUBLIC_KEY } from 'src/decorators/isPublic';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    /**  skip public routes -  via @Public our custom decorator  */
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
@@ -22,6 +36,7 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
+
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
