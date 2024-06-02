e7d9937d6af7e597652f933129fe0d29dc7aad63
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-02 08:47:40 +0200

    used configService inside main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import helmet from 'helmet';
 import * as compression from 'compression';
-
-const PORT = parseInt(process.env.PORT, 10) || 4000;
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
   // register plugins and extensions
   app.enableCors({ origin: '*' }); // for production: specific IP
   app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({ type: VersioningType.URI });
   app.use(helmet());
   app.use(compression());
+
+  const PORT = configService.get('PORT') || 4000;
 
   await app.listen(PORT, () =>
     Logger.log(`🚀 Server is listening on PORT:${PORT}`),
