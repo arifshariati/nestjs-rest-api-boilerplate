8d83ff88e0ad05cf049056bdbae927f819f0c717
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-02 11:50:17 +0200

    refactored user contoller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,26 +1,18 @@
 import {
   Controller,
   Get,
-  Post,
   Body,
   Patch,
   Param,
   Delete,
-  HttpException,
-  HttpStatus,
+  NotFoundException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.userService.create(createUserDto);
-  }
 
   @Get()
   async findAll() {
@@ -29,20 +21,24 @@ export class UserController {
 
   @Get(':id')
   async findById(@Param('id') id: string) {
-    return await this.userService.findById(id);
+    const userData = await this.userService.findById(id);
+    if (!userData) throw new NotFoundException();
+    return userData;
   }
 
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const userData = await this.findById(id);
-    if (!userData)
-      throw new HttpException('Requested data not found', HttpStatus.NOT_FOUND);
+    if (!userData) throw new NotFoundException();
 
     return await this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string) {
+    const userData = await this.findById(id);
+    if (!userData) throw new NotFoundException();
+
     return await this.userService.remove(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,35 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from './schema/user.schema';
+import { User, UserDocument } from './schema/user.schema';
+import { SignUpDto } from 'src/types/dtos/signup.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async create(createUserDto: SignUpDto): Promise<UserDocument> {
     const userCreated = new this.userModel(createUserDto);
-    return userCreated.save();
+    return await userCreated.save();
   }
 
   async findAll(): Promise<User[]> {
     return this.userModel.find({}).exec();
   }
 
-  async findById(id: string): Promise<User> {
-    return this.userModel.findById(id);
+  async findById(id: string): Promise<UserDocument> {
+    return await this.userModel.findById(id).exec();
   }
-  async findOne(field: Record<string, any>): Promise<User> {
-    return this.userModel.findOne(field);
+  async findOne(field: Record<string, any>): Promise<UserDocument> {
+    return await this.userModel.findOne(field).exec();
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+  async update(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserDocument> {
     return this.userModel.findByIdAndUpdate(id, updateUserDto).exec();
   }
 
-  async remove(id: string): Promise<User> {
+  async remove(id: string): Promise<UserDocument> {
     return this.userModel.findByIdAndDelete(id);
   }
 }
