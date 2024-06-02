63df3fae0edab6cc02447d92fb10b5d560ec1dea
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-06-02 11:50:50 +0200

    added signup function in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { SignInDto } from 'src/types/dtos/signin.dto';
+import { SignUpDto } from 'src/types/dtos/signup.dto';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
@@ -8,16 +15,40 @@ export class AuthService {
     private userService: UserService,
     private jwtService: JwtService,
   ) {}
-  async signIn(
-    username: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
-    const user = await this.userService.findOne({ username });
-    if (user?.password !== pass) {
-      throw new UnauthorizedException();
+  async signIn({
+    email,
+    password,
+  }: SignInDto): Promise<{ access_token: string } | UnauthorizedException> {
+    const user = await this.userService.findOne({ email });
+
+    if (!user) throw new UnauthorizedException();
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatching) {
+      throw new UnauthorizedException('Invalid password');
     }
 
-    const payload = { sub: user.userId, username: user.username };
+    const payload = { sub: user._id, email: user.email };
+    return { access_token: await this.jwtService.signAsync(payload) };
+  }
+
+  async signUp({
+    email,
+    password,
+    ...rest
+  }: SignUpDto): Promise<{ access_token: string } | UnauthorizedException> {
+    const user = await this.userService.findOne({ email });
+    if (user) throw new BadRequestException('User already exists');
+
+    const hanshedPassword = await bcrypt.hash(password, 10);
+    const createdUser = await this.userService.create({
+      email,
+      password: hanshedPassword,
+      ...rest,
+    });
+
+    if (!createdUser) throw new UnauthorizedException();
+
+    const payload = { sub: createdUser._id, email: createdUser.email };
     return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
